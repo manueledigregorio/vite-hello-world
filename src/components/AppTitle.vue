aaf6fa2d6cf4115a6ca97dea738cd5b14542d189
manueledigregorio <dig.manuele@gmail.com>
2023-10-09 18:35:38 +0200

    [ADD] title

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+ 
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name:'AppTitle',
+  data() {
+    return {
+      title: 'la mia prima app con  Vite!',
+    }
+  },
+  
+}
+</script>
+
+<style>
+
+</style>
